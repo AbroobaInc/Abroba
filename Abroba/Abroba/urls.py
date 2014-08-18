9ad4366c0437a50805b736cc2064d4237858633d
Fabio <ayresfj@gmail.com>
2014-08-18 20:10:03 -0300

    outro teste

--- Abroba/Abroba/urls.py
+++ Abroba/Abroba/urls.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'Abroba.views..home', name='home'),
+    # Examples: abrooba!
+    # url(r'^$', 'Abroba.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
