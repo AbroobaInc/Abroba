0ecbf6265de2d8b6fb06b021c8e101c9e31cb985
Fabio <ayresfj@gmail.com>
2014-08-18 20:01:27 -0300

    Teste

--- Abroba/Abroba/urls.py
+++ Abroba/Abroba/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'Abroba.views.home', name='home'),
+    # url(r'^$', 'Abroba.views..home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
